102c81449fcc7a49a0e2a128eb8b4f38acb72ad5
DilanLahiru <dilanlahiruijse@gmail.com>
2026-03-25 12:35:22 +0530

    fix: update base URL for the API to the correct server address

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,6 +1,6 @@
 // Define the base URL for the API
-//export const baseUrl = "http://143.20.170.42:3000";
-export const baseUrl = "http://192.168.8.101:3000";
+export const baseUrl = "http://143.20.170.42:3000";
+//export const baseUrl = "http://192.168.8.101:3000";
 
 export const API_PATH = {
   AUTH: {
